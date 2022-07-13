95c6aaf479ae5e25d30b476f6668d668e3df971e
mparvathaneni <mparvathaneni@woolworths.com.au>
2022-07-13 14:28:27 +1000

    updated

--- Example1-VariableTypes/main.tf
+++ Example1-VariableTypes/main.tf
@@ -6,3 +6,12 @@ resource "azurerm_resource_group" "rg" {
   name     = "TestRG"
   location = "AustraliaEast"
 }
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "My-Terraform-Sample"
+    storage_account_name = "tfbackend009"
+    container_name       = "terraformstate"
+    key                  = "test.tfstate"
+  }
+}
